96caec89d60d2854db5ce6ea40160d0589a6fca0
Clon <clonvhs@gmail.com>
2021-05-28 11:44:11 +0300

    #15 lesson: - add SinglePost.js component and import it on MyProfilePage.js - add some styles -fil MyProfilePage.js with some text inside tags

--- src/MyProfilePage.js
+++ src/MyProfilePage.js
@@ -1,31 +1,36 @@
 import userMainPageBackgroundPhoto from './images/userMainPageBackgroundPhoto.jpeg'
+import userProfileImage from './images/userProfileImage.png'
 import styles from './MyProfilePage.module.css'
+import SinglePost from './SinglePost'
 
 const MyProfilePage = () => {
     return (<div>
         <img className={styles.userMainPageBackgroundPhoto} src={userMainPageBackgroundPhoto}/>
         <div className={styles.userInfoBlock}>
-            <div className={styles.userAvatar}>user Ava</div>
-            <div className={styles.userName}></div>
-            <div className={styles.descriptionBlock}>
-                <div className={styles.bitrhDate}></div>
-                <div className={styles.userCity}></div>
-                <div className={styles.userEducation}></div>
-                <div className={styles.userWebSite}></div>
-            </div>
-            <div className={styles.userPostsBlock}>
-                <div className={styles.userPostsTittle}>My posts</div>
-                <div className={styles.userSendNewPostBlock}>
-                    <div className={styles.newPostInput}>NewPost</div>
-                    <div className={styles.newPostButton}></div>
-                </div>
-                <div className={styles.postsTapeBlock}>
-                    post1
-                    <br/>
-                    post2
+            <div className={styles.userInfoBlockLeftSide}><img className={styles.userAvatar} src={userProfileImage}
+                                                               alt="user profile image"/></div>
+            <div className={styles.userInfoBlockRightSide}>
+                <div className={styles.userName}>VoVa B.</div>
+                <div className={styles.descriptionBlock}>
+                    <div className={styles.bitrhDate}>13 october</div>
+                    <div className={styles.userCity}>Khmelnitskiy</div>
+                    <div className={styles.userEducation}>KUoL</div>
+                    <div className={styles.userWebSite}><a href={'https://youtube.com'}> https://youtube.com</a></div>
                 </div>
             </div>
         </div>
+        <div className={styles.userPostsBlock}>
+            <div className={styles.userPostsTittle}>My posts</div>
+            <div className={styles.userSendNewPostBlock}>
+                <input className={styles.newPostInput} value={'New Post'}/>
+                <button className={styles.newPostButton}>Send new post</button>
+            </div>
+            <div className={styles.postsTapeBlock}>
+                <SinglePost/>
+                <SinglePost/>
+            </div>
+        </div>
+
 
     </div>)
 }
---
+++ src/SinglePost.js
@@ -0,0 +1,11 @@
+import styles from './SinglePost.module.css'
+import postUserAvatarImage from "./images/postUserAvatarImage.png";
+
+const SinglePost = () =>{
+    return(<div className={styles.mainWrapper}>
+        <img className={styles.postUserAvatarImage} src={postUserAvatarImage}/>
+        <p className={styles.postText}>Hello, how are you</p>
+    </div>)
+}
+
+export default SinglePost
