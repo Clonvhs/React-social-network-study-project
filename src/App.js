f2c8f81d3349f1a9b56394c30542f6a670b7f213
Clon <clonvhs@gmail.com>
2021-06-01 18:48:34 +0300

    #19 lesson: - Add npm install react-router-dom -save package - Add BrowserRouter and import it - Add Route and some attributes to hem

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from './Header/Header'
 import MyProfilePage from "./Profile/MyProfilePage";
 import NavigationMenu from "./NavigationMenu/NavigationMenu";
 import Dialogs from "./Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
-  return (<div>
-      <Header/>
-      <div className="main-page-main-countent-wrapper">
-          <NavigationMenu/>
-          <Dialogs/>
-          {/*<MyProfilePage/>*/}
-      </div>
+    return (<BrowserRouter>
+        <div>
+            <Header/>
+            <div className="main-page-main-content-wrapper">
+                <NavigationMenu/>
+                <Route path='/dialogs' component={Dialogs} />
+                <Route path='/profile' component={MyProfilePage} />
+            </div>
 
-      </div>);
+        </div>
+    </BrowserRouter>);
 }
 
 export default App;
--- src/NavigationMenu/NavigationMenu.js
+++ src/NavigationMenu/NavigationMenu.js
@@ -2,8 +2,8 @@ import styles from './NavigationMenu.module.css'
 const NavigationMenu = () =>{
     return(<div className={styles.mainWrapper}>
         <ul>
-            <li><a>Profile</a></li>
-            <li><a>Messages</a></li>
+            <li><a href='/profile'>Profile</a></li>
+            <li><a href='/dialogs'>Messages</a></li>
             <li><a>News</a></li>
             <li><a>Music</a></li>
             <li><a>Settings</a></li>
