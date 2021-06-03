d3b97619fb73076839ebe62971d99454e8f27cd5
Clon <clonvhs@gmail.com>
2021-06-03 18:44:14 +0300

    #21 lesson: - Add some HTML elements to Dialogs.js and some CSS styles

--- src/Dialogs/Dialogs.js
+++ src/Dialogs/Dialogs.js
@@ -3,7 +3,23 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (<div className={styles.mainWrapper} >
-        Dialogs
+        <div className="title"></div>
+        <div className={styles.separatorLeftSideBlock}>
+            <div className={styles.userListBlock}>
+                <div className={styles.userDialogName}>Dimych</div>
+                <div className={styles.userDialogName}>Andrey</div>
+                <div className={styles.userDialogName}>Sveta</div>
+                <div className={styles.userDialogName}>Sasha</div>
+                <div className={styles.userDialogName}>Masha</div>
+                <div className={styles.userDialogName}>Ivan</div>
+                <div className={styles.userDialogName}>Ivasdadadadadasdasdasdasdadsadadasdasdasdan</div>
+            </div>
+            <div className={styles.dialogBlock}>
+                <div className={styles.messageItem}>hi</div>
+                <div className={styles.messageItem}>Hi may mate</div>
+                <div className={styles.messageItem}>how Are you?</div>
+            </div>
+        </div>
     </div>)
 }
 
